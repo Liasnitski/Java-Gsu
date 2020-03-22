a651837228ed64a2a015cb0a64c7cada8b82f27f
Liasnitski <liasnitski00@gmail.com>
2020-03-22 15:47:40 +0300

    add all files

--- lab_2_inter_abstr/src/by/gsu/pms/Banks.java
+++ lab_2_inter_abstr/src/by/gsu/pms/Banks.java
@@ -1,4 +1,10 @@
 package by.gsu.pms;
 
-public class Banks {
+public class Banks extends Grains {
+
+    public Banks() { super(); }
+
+    public Banks(String coffeeName, float coffeeSize) { super(coffeeName, coffeeSize); }
+
+    public Banks(String coffeeName, float coffeeSize, int coffeePrice) { super(coffeeName, coffeeSize, coffeePrice);}
 }
--- lab_2_inter_abstr/src/by/gsu/pms/Grains.java
+++ lab_2_inter_abstr/src/by/gsu/pms/Grains.java
@@ -1,4 +1,38 @@
 package by.gsu.pms;
 
-public class Grains {
+
+abstract class Grains implements Coffees {
+
+    private String coffeeName;
+    private float coffeeSize;
+    private int coffeePrice;
+
+    public String getCoffeeName() { return coffeeName; }
+
+    public float getCoffeeSize() { return coffeeSize; }
+
+    public int getCoffeePrice() { return coffeePrice; }
+
+    public void setCoffeeName(String coffeeName) { this.coffeeName = coffeeName; }
+
+    public void setCoffeePrice(int coffeePrice) { this.coffeePrice = coffeePrice; }
+
+    public void setCoffeeSize(float coffeeSize) { this.coffeeSize = coffeeSize; }
+
+    public Grains() {}
+
+    public Grains(String coffeeName, float coffeeSize) {
+        this.coffeeName = coffeeName;
+        this.coffeeSize = coffeeSize;
+    }
+
+    public Grains(String coffeeName, float coffeeSize, int coffeePrice) {
+        this.coffeeName = coffeeName;
+        this.coffeeSize = coffeeSize;
+        this.coffeePrice = coffeePrice;
+    }
+
+    public int compareTo(Coffees compareCoffee) {
+        return this.coffeePrice - compareCoffee.getCoffeePrice();
+    }
 }
